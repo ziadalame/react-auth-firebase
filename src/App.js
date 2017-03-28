9cafed580370a6460f9f7877b45f4dcbdec79535
ziad <ziadalame@gmail.com>
2017-03-29 01:10:09 +0300

    Added common components - Initized Firebase

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 //import liraries
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 // create a component
 class App extends Component {
+
+    componentWillMount() {
+        // Initialize Firebase
+        firebase.initializeApp({
+            apiKey: 'AIzaSyAf2xMF9v3hk_NYW2CT87PoCPSOawZgMJ4',
+            authDomain: 'react-native-test-68ab6.firebaseapp.com',
+            databaseURL: 'https://react-native-test-68ab6.firebaseio.com',
+            storageBucket: 'react-native-test-68ab6.appspot.com',
+            messagingSenderId: '227416283478'
+        });
+    }
+
     render() {
         return (
-            <View style={styles.container}>
+            <View>
+                <Header headerText="Authentication" />
                 <Text>App</Text>
             </View>
         );
     }
 }
-
-// define your styles
-const styles = {
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50'
-    }
-};
 
 //make this component available to the app
 export default App;
